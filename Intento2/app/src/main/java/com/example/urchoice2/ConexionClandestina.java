a3e46b572e6280ee91245879f207fee837966f86
serijio <serijiomainsett@gmail.com>
2024-04-14 20:39:59 +0200

    actualizao

--- Intento2/app/src/main/java/com/example/urchoice2/ConexionClandestina.java
+++ Intento2/app/src/main/java/com/example/urchoice2/ConexionClandestina.java
@@ -11,32 +11,38 @@ import java.util.Properties;
 
 public class ConexionClandestina extends AsyncTask<Void,Integer,Boolean> {
 
-    String queryResult;
+    String queryResult; //esto es pa meterle el valor en String de cualquier consulta tipo SELECT
 
     @Override
     protected Boolean doInBackground(Void... params) {
         try {
-            //String connectionString = "jdbc:mysql://monorail.proxy.rlwy.net:45523/railway;user=root;password=vDemhtCuWGRNzlYnkENndIbEtZsnnylA";
+            /*En railway, es la que aparece en "Settings" en el apartado de NetWorking, poniéndole delante "jdbc:mysql://"
+            y detrás el nombre que aparece en "Variables" como "MYSQLDATABASE" con una barra delante, en este caso, "/railway" */
             String connectionString = "jdbc:mysql://monorail.proxy.rlwy.net:45523/railway";
-            //String connectionString = "jdbc:jtds:mysql://monorail.proxy.rlwy.net:45523/railway";
+
+            /*Lo que aparece como "MYSQLUSER" en la parte de "Variables", es el súper usuario, lo mantenemos siempre así */
             String user = "root";
+
+            /*Lo que aparece como "MYSQL_ROOT_PASSWORD" en la parte de "Variables" */
             String password = "vDemhtCuWGRNzlYnkENndIbEtZsnnylA";
-            /*Properties props = new Properties();
-            props.setProperty("user", "root");
-            props.setProperty("password", "vDemhtCuWGRNzlYnkENndIbEtZsnnylA");*/
+
             Connection con = DriverManager.getConnection(connectionString, user, password);
             if (con == null)
             {
                 return false;
             }
 
+            /*Consulta tipo SELECT, tiene:
+                - Statement: pa crear la consulta
+                - ResultSet: pa decirle de que va la consulta, siempre es "executeQuery"
+                - StringBuilder: pa convertir lo que devuelva el SELECT a un String
+                - */
             Statement statement = con.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
 
             StringBuilder resultStringBuilder = new StringBuilder();
 
             while (resultSet.next()) {
-                // Assuming you have a column named 'column_name' in your table
                 String id_user = resultSet.getString("id_user");
                 String email_user = resultSet.getString("email_user");
                 String nick_user = resultSet.getString("nick_user");
@@ -49,35 +55,33 @@ public class ConexionClandestina extends AsyncTask<Void,Integer,Boolean> {
                 resultStringBuilder.append(img_user).append("\n");
             }
 
-            /*if (resultSet.first()) {
-                do {
-                    String id_user = resultSet.getString("id_user");
-                    Log.d("USER","id: " + id_user);
-                    String email_user = resultSet.getString("email_user");
-                    String nick_user = resultSet.getString("nick_user");
-                    String pass_user = resultSet.getString("pass_user");
-                    String img_user = resultSet.getString("img_user");
-                    resultStringBuilder.append(id_user).append("\n");
-                    resultStringBuilder.append(email_user).append("\n");
-                    resultStringBuilder.append(nick_user).append("\n");
-                    resultStringBuilder.append(pass_user).append("\n");
-                    resultStringBuilder.append(img_user).append("\n");
-                } while (resultSet.next()); // Move to the next row if available
-            }*/
-
-            // Assign the result to the queryResult variable
+            // Asignarle al String el valor de la consulta
             queryResult = resultStringBuilder.toString();
-            //Log.d("WENAS query Result:", "jiji " + queryResult);
 
-            Statement statement2 = con.createStatement();
-            int rowsAffected = statement2.executeUpdate("INSERT INTO users VALUES(" +
-                    "'2','alexgei','supergei','1234','');");
+            /*Consulta tipo INSERT o DELETE, tiene:
+                - Statement: pa crear la consulta
+                - int: siempre va con el "executeUpdate"
 
-            if (rowsAffected > 0) {
-                // Insertion was successful
+                Ejemplos de consultas:
+                  Statement statement2 = con.createStatement();
+                  int rowsAffected2 = statement2.executeUpdate("INSERT INTO users VALUES(" +
+                                                              "'2','alexgei','supergei','1234','');");
+
+
+                  Statement statement3 = con.createStatement();
+                  int rowsAffected3 = statement3.executeUpdate("INSERT INTO users VALUES(" +
+                                                               "'3','lucajin','pelucas','1234','');");
+                */
+
+            Statement statement4 = con.createStatement();
+            int rowsAffected4 = statement4.executeUpdate("INSERT INTO users VALUES(" +
+                    "'4','kike','kiko','rocky','');");
+
+            if (rowsAffected4 > 0) {
+                // Pa que luego pongamos el típico mensaje de "Cuenta creada"
                 Log.d("ANIADIR", "Mu bien, has agregao a Alex gei");
             } else {
-                // Insertion failed
+                // Por ejemplo pa decirle "Tienes que meter todos tus datos mimimimi"
                 Log.d("ANIADIR", "Alex es tan gei que no se deja agregar :(");
             }
 
