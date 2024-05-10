23f94c62f6c9b36eec0a12fd7f8ad4f384b782c9
serijio <serijiomainsett@gmail.com>
2024-05-10 22:26:39 +0200

    push
    push

---
+++ Intento2/app/src/main/java/com/example/urchoice2/Fragments/MainRankingSubFragment.java
@@ -0,0 +1,44 @@
+package com.example.urchoice2.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.urchoice2.R;
+
+public class MainRankingSubFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public MainRankingSubFragment() {}
+
+    public static MainRankingSubFragment newInstance(String param1, String param2) {
+        MainRankingSubFragment fragment = new MainRankingSubFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_ranking_sub, container, false);
+    }
+}
