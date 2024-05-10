db68d25c168515d84b1cab0082db558d8a996d2b
LordGrim551 <lucajin551@gmail.com>
2024-05-11 00:31:32 +0200

    MAS COSITAS

--- Intento2/app/src/main/java/com/example/urchoice2/Adapters/MainFragment_Room_Adapter.java
+++ Intento2/app/src/main/java/com/example/urchoice2/Adapters/MainFragment_Room_Adapter.java
@@ -23,12 +23,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.urchoice2.API.RoomAPI;
 import com.example.urchoice2.Classes.RoomData;
 import com.example.urchoice2.Classes.Rooms;
+import com.example.urchoice2.Classes.UserVote;
 import com.example.urchoice2.R;
 import com.example.urchoice2.Screens_activities.prueba;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -39,11 +43,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainFragment_Room_Adapter extends RecyclerView.Adapter<MainFragment_Room_Adapter.RoomViewHolder> {
 
     private Context context;
-
-
-
+    private boolean shouldUpdate = true;
     private List<Rooms> rooms;
     private RoomAPI roomAPI;
+    private List<UserVote> userVotes  = new ArrayList<>();
+    private int roomId;
+    private int userId;
 
 
 
@@ -65,7 +70,7 @@ public class MainFragment_Room_Adapter extends RecyclerView.Adapter<MainFragment
     public void onBindViewHolder(@NonNull RoomViewHolder holder, @SuppressLint("RecyclerView") int position) {
         Conectar();
         SharedPreferences preferences = context.getSharedPreferences("UrChoice", Context.MODE_PRIVATE);
-        int userId = preferences.getInt("id_user", 0);
+        userId = preferences.getInt("id_user", 0);
 
         String roomName = rooms.get(position).getName_room();
         int numberOfPlayers = this.rooms.get(position).getUserCount();
@@ -83,18 +88,13 @@ public class MainFragment_Room_Adapter extends RecyclerView.Adapter<MainFragment
         }
 
 
-
-
-
         // Agregar OnClickListener al TextView del nombre de la habitación
         holder.roomNameTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
-
                 // Aquí abres el AlertDialog
-                int roomId = rooms.get(position).getId_room();
+                roomId = rooms.get(position).getId_room();
 
                 if(!rooms.get(position).getPass_room().isEmpty()){
                     LayoutInflater inflater = LayoutInflater.from(context);
@@ -109,8 +109,6 @@ public class MainFragment_Room_Adapter extends RecyclerView.Adapter<MainFragment
                     alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
                     alertDialog.show();
 
-
-
                     // Obtener referencia al botón dentro del AlertDialog
                     MaterialButton enterRoomPinButton = dialogView.findViewById(R.id.enter_room_pin);
                     enterRoomPinButton.setOnClickListener(new View.OnClickListener() {
@@ -122,10 +120,6 @@ public class MainFragment_Room_Adapter extends RecyclerView.Adapter<MainFragment
                             alertDialog.dismiss();
                             joinRoom(roomId,userId,pinValue);
                             // Abrir el nuevo AlertDialog f3__x__fragment_alert_waiting_players
-
-
-
-
                         }
                     });
 
@@ -176,6 +170,152 @@ public class MainFragment_Room_Adapter extends RecyclerView.Adapter<MainFragment
         });
     }
 
+    public void Conectar(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://railwayserver-production-7692.up.railway.app")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        roomAPI = retrofit.create(RoomAPI.class);
+    }
+
+    public void Room(){
+        shouldUpdate = true;;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View waitingPlayersDialogView = inflater.inflate(R.layout.f3__x__fragment_alert_waiting_players, null);
+
+        RecyclerView recyclerView = waitingPlayersDialogView.findViewById(R.id.recycler_players);
+
+
+        recyclerView.setAdapter(new RecyclerView.Adapter() {
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.f3__x__fragment_player_status_cardview, parent, false);
+                return new RecyclerView.ViewHolder(itemView) {
+                };
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+                TextView playerName = holder.itemView.findViewById(R.id.player_name);
+                ImageView readyicon = holder.itemView.findViewById(R.id.ready_status);
+                MaterialButton exitstatus = holder.itemView.findViewById(R.id.exit_status);
+                Log.e("SQL","Tamaño " + userVotes.size());
+                boolean admin = false;
+                playerName.setText(userVotes.get(position).getNick_user());
+                for(int i = 0;i < userVotes.size();i++){
+                    Log.e("SQL","Nombre: " + userVotes.get(i).getNick_user());
+                    if(userVotes.get(i).getId_user() == userId && userVotes.get(i).getAdmin() == 1){
+                        admin = true;
+                    }
+                }
+                if(userVotes.get(position).getVote_game().equals("LISTO")){
+                    readyicon.setVisibility(View.VISIBLE);
+                }
+                if(admin == true){
+                    exitstatus.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public int getItemCount() {
+                return userVotes.size();
+            }
+        });
+        UsersRoom(recyclerView.getAdapter());
+
+        // Construir el nuevo AlertDialog
+        AlertDialog.Builder waitingPlayersBuilder = new AlertDialog.Builder(context);
+        waitingPlayersBuilder.setView(waitingPlayersDialogView);
+
+        // Mostrar el nuevo AlertDialog
+        AlertDialog waitingPlayersAlertDialog = waitingPlayersBuilder.create();
+        waitingPlayersAlertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        waitingPlayersAlertDialog.show();
+        MaterialButton exitButton = waitingPlayersDialogView.findViewById(R.id.alert_exit_button);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shouldUpdate = false;
+                endRoom(roomId,userId);
+                waitingPlayersAlertDialog.dismiss();
+            }
+        });
+        MaterialButton startButton = waitingPlayersDialogView.findViewById(R.id.alert_start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(context, prueba.class);
+
+                        // Verificar si la versión de Android es igual o superior a LOLLIPOP para manejar la transición de actividades
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                            startButton.setText("ESPERANDO AL ANFITRIÓN");
+                            startButton.setTextSize(10);
+
+
+                            Listo(roomId,userId);
+
+                            //context.startActivity(intent);
+                        } else {
+                            // En versiones anteriores a LOLLIPOP, simplemente inicia la actividad
+                            context.startActivity(intent);
+                            // Si es necesario, finaliza la actividad actual
+                            // ((Activity) context).finish();
+                        }
+                    }
+                }, 400);
+            }
+
+        });
+    }
+
+    public void UsersRoom(RecyclerView.Adapter adapter) {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (!shouldUpdate) {
+                    // Si la variable shouldUpdate es falsa, cancelar la tarea del temporizador
+                    timer.cancel();
+                    return;
+                }
+                Call<List<UserVote>> call = roomAPI.getUsersInRoom(roomId);
+                call.enqueue(new Callback<List<UserVote>>() {
+                    @Override
+                    public void onResponse(Call<List<UserVote>> call, Response<List<UserVote>> response) {
+                        if (response.isSuccessful()) {
+                            userVotes = response.body();
+                            List<UserVote> newNames = new ArrayList<>();
+                            for (UserVote userVote : userVotes) {
+                                newNames.add(userVote);
+                                Log.e("SQL", "User: " + userVote.getNick_user());
+                            }
+                            updateAdapter(newNames,adapter); // Actualiza el adaptador con los nuevos nombres de usuario
+                        } else {
+                            // Manejar respuesta no exitosa
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<UserVote>> call, Throwable t) {
+                        // Manejar error de red u otro error
+                    }
+                });
+            }
+        }, 0, 5000); // Ejecutar la tarea cada 5 segundos
+    }
+
+    private void updateAdapter(List<UserVote> userVote, RecyclerView.Adapter adapter) {
+        if (!shouldUpdate) {
+            return;
+        }
+        userVotes.clear();
+        userVotes.addAll(userVote);
+        adapter.notifyDataSetChanged();
+    }
+
     public void endRoom(int roomId, int userId) {
         Call<Void> call = roomAPI.endRoom(roomId, userId);
         call.enqueue(new Callback<Void>() {
@@ -196,89 +336,25 @@ public class MainFragment_Room_Adapter extends RecyclerView.Adapter<MainFragment
             }
         });
     }
-    public void Conectar(){
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://railwayserver-production-7692.up.railway.app")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        roomAPI = retrofit.create(RoomAPI.class);
-    }
 
-    public void Room(){
-
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View waitingPlayersDialogView = inflater.inflate(R.layout.f3__x__fragment_alert_waiting_players, null);
-
-        RecyclerView recyclerView = waitingPlayersDialogView.findViewById(R.id.recycler_players);
-        String[] usernames = {"TheRockex", "Spidey1912", "Lukinda551", "LordGrim551", "TuMama", "王八蛋"};
-
-
-        recyclerView.setAdapter(new RecyclerView.Adapter() {
-            @NonNull
+    public void Listo(int roomId, int userId){
+        Call<Void> call = roomAPI.startRoom(roomId, userId);
+        call.enqueue(new Callback<Void>() {
             @Override
-            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.f3__x__fragment_player_status_cardview, parent, false);
-                return new RecyclerView.ViewHolder(itemView) {
-                };
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    Log.d("RoomEnd", "OperaciÃ³n completada correctamente");
+                } else {
+                    // OcurriÃ³ un error al intentar finalizar la sala
+                    Log.e("RoomEnd", "Error al finalizar la sala: " + response.message());
+                }
             }
 
             @Override
-            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-                TextView playerName = holder.itemView.findViewById(R.id.player_name);
-                ImageView readyicon = holder.itemView.findViewById(R.id.ready_status);
-                MaterialButton exitstatus = holder.itemView.findViewById(R.id.exit_status);
-
-                playerName.setText(usernames[position]);
-                readyicon.setVisibility(View.VISIBLE);
-                exitstatus.setVisibility(View.VISIBLE);
+            public void onFailure(Call<Void> call, Throwable t) {
+                // OcurriÃ³ un error de red tap_blue_card otro error durante la llamada
+                Log.e("RoomEnd", "Error de red: " + t.getMessage());
             }
-
-            @Override
-            public int getItemCount() {
-                return usernames.length;
-            }
-        });
-
-
-        // Construir el nuevo AlertDialog
-        AlertDialog.Builder waitingPlayersBuilder = new AlertDialog.Builder(context);
-        waitingPlayersBuilder.setView(waitingPlayersDialogView);
-
-        // Mostrar el nuevo AlertDialog
-        AlertDialog waitingPlayersAlertDialog = waitingPlayersBuilder.create();
-        waitingPlayersAlertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
-        waitingPlayersAlertDialog.show();
-        MaterialButton exitButton = waitingPlayersDialogView.findViewById(R.id.alert_exit_button);
-        exitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                waitingPlayersAlertDialog.dismiss();
-            }
-        });
-        MaterialButton startButton = waitingPlayersDialogView.findViewById(R.id.alert_start_button);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent intent = new Intent(context, prueba.class);
-
-                        // Verificar si la versión de Android es igual o superior a LOLLIPOP para manejar la transición de actividades
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                            startButton.setText("ESPERANDO AL ANFITRIÓN");
-                            startButton.setTextSize(10);
-                            //context.startActivity(intent);
-                        } else {
-                            // En versiones anteriores a LOLLIPOP, simplemente inicia la actividad
-                            context.startActivity(intent);
-                            // Si es necesario, finaliza la actividad actual
-                            // ((Activity) context).finish();
-                        }
-                    }
-                }, 400);
-            }
-
         });
     }
 }
