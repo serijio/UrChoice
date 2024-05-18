fb77276c814c59bbc992bb9c504f4f408f7027ce
serijio <serijiomainsett@gmail.com>
2024-05-18 19:22:37 +0200

    altera
    gif

---
+++ Intento2/app/src/main/java/com/example/urchoice2/Fragments/EditCategoriesSubFragment.java
@@ -0,0 +1,44 @@
+package com.example.urchoice2.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.urchoice2.R;
+
+public class EditCategoriesSubFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+
+    public EditCategoriesSubFragment() {}
+
+    public static EditCategoriesSubFragment newInstance(String param1, String param2) {
+        EditCategoriesSubFragment fragment = new EditCategoriesSubFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.f5___x_sub__fragment_profile__edit_categories, container, false);
+        return view;
+    }
+}
